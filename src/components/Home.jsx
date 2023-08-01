9286f83afe278eab0132ea8cd504f237ab5c61ae
jkentwhite <jkentwhite@gmail.com>
2023-08-01 15:38:46 -0500

    functioning api call

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,29 +1,11 @@
 import React from 'react';
 import axios from 'axios';
 
-const Home  = ({baseURL, API_KEY, headers}) => {
-    
-  const [post, setPost] = React.useState(null);
+const Home  = () => {
 
-  React.useEffect((baseURL, headers) => {
-
-    axios.get(baseURL, headers).then((response) => {
-      setPost(response.data);
-    });
-  }, []);  
-  
-  if(!post) return null;
-
-
-  
     return (
         <div className="content">
             <h1 className="main-title">Informed Citizen</h1>
-            <ul>
-                {post.map(member => (
-                <li key={member.id}>{member.name} ({member.party})</li>
-                ))}
-            </ul>
         </div>
       );
 }
--- src/components/Representatives.jsx
+++ src/components/Representatives.jsx
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-const Representatives  = () => {
-    return (
-        <div className="content">
-            <h2 className="main-title">Representatives</h2>
-        </div>
-      );
-}
- 
+const Representatives = () => {
+  const [members, setMembers] = useState([]);
+
+  useEffect(() => {
+    const apiKey = process.env.REACT_APP_PROPUBLICA_API_KEY; // Replace with your actual API key
+    const apiUrl = 'https://api.propublica.org/congress/v1/116/senate/members.json';
+
+    const config = {
+      headers: {
+        'X-API-Key': apiKey,
+      },
+    };
+
+    axios
+      .get(apiUrl, config)
+      .then((response) => {
+        setMembers(response.data.results[0].members);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
+  console.log(members);
+
+  return (
+    <div>
+      <h1>Congress Members from New York</h1>
+      <ul>
+        {members.map((member) => (
+          <li key={member.id}>
+            {member.first_name} {member.last_name} ({member.party}) - {member.role}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
 export default Representatives;
